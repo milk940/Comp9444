8219c3ac13baf9b25b67adac646ba0c22b91ca5a
Tingjun Wu <z5148571@cse.unsw.edu.au>
2018-09-13 22:09:35 +1000

    Set-up

---
+++ implementation.py
@@ -0,0 +1,56 @@
+import tensorflow as tf
+
+BATCH_SIZE = 128
+MAX_WORDS_IN_REVIEW = 100  # Maximum length of a review to consider
+EMBEDDING_SIZE = 50  # Dimensions for each word vector
+
+stop_words = set({'ourselves', 'hers', 'between', 'yourself', 'again',
+                  'there', 'about', 'once', 'during', 'out', 'very', 'having',
+                  'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its',
+                  'yours', 'such', 'into', 'of', 'most', 'itself', 'other',
+                  'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him',
+                  'each', 'the', 'themselves', 'below', 'are', 'we',
+                  'these', 'your', 'his', 'through', 'don', 'me', 'were',
+                  'her', 'more', 'himself', 'this', 'down', 'should', 'our',
+                  'their', 'while', 'above', 'both', 'up', 'to', 'ours', 'had',
+                  'she', 'all', 'no', 'when', 'at', 'any', 'before', 'them',
+                  'same', 'and', 'been', 'have', 'in', 'will', 'on', 'does',
+                  'yourselves', 'then', 'that', 'because', 'what', 'over',
+                  'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you',
+                  'herself', 'has', 'just', 'where', 'too', 'only', 'myself',
+                  'which', 'those', 'i', 'after', 'few', 'whom', 't', 'being',
+                  'if', 'theirs', 'my', 'against', 'a', 'by', 'doing', 'it',
+                  'how', 'further', 'was', 'here', 'than'})
+
+def preprocess(review):
+    """
+    Apply preprocessing to a single review. You can do anything here that is manipulation
+    at a string level, e.g.
+        - removing stop words
+        - stripping/adding punctuation
+        - changing case
+        - word find/replace
+    RETURN: the preprocessed review in string form.
+    """
+
+    return processed_review
+
+
+
+def define_graph():
+    """
+    Implement your model here. You will need to define placeholders, for the input and labels,
+    Note that the input is not strings of words, but the strings after the embedding lookup
+    has been applied (i.e. arrays of floats).
+
+    In all cases this code will be called by an unaltered runner.py. You should read this
+    file and ensure your code here is compatible.
+
+    Consult the assignment specification for details of which parts of the TF API are
+    permitted for use in this function.
+
+    You must return, in the following order, the placeholders/tensors for;
+    RETURNS: input, labels, optimizer, accuracy and loss
+    """
+
+    return input_data, labels, dropout_keep_prob, optimizer, Accuracy, loss
